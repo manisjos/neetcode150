96b77ace1e3c41c01930024899453f9ddffd8799
manisjos <manish.joshi.d@gmail.com>
2025-08-03 22:01:27 +0530

    <><printing cousing binary tree>

---
+++ CousinsBinaryTree.java
@@ -0,0 +1,36 @@
+public class CousinsBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        root.right.right = new TreeNode(7);
+        root.right.left = new TreeNode(6);
+
+        TreeNode target = root.left.right;
+        System.out.print("Cousins of node " + target.data + " ");
+        printCousins(root, target);
+    }
+
+    private static void printCousins(TreeNode root, TreeNode target) {
+        int level = getLevel(root, target, 1);
+        printGivenLevelExceptSibling(root, target, level);
+    }
+
+    private static int getLevel(TreeNode root, TreeNode target, int level) {
+        if (root == null) return 0;
+        if (root == target) return level;
+
+        int leftLevel = getLevel(root.left, target, level + 1);
+        if (leftLevel != 0) return leftLevel;
+
+        return getLevel(root.right, target, level + 1);
+    }
+
+    private static void printGivenLevelExceptSibling(TreeNode root, TreeNode target, int level) {
+
+    }
+}
---
+++ TreeNode.java
@@ -0,0 +1,9 @@
+public class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int value) {
+        data = value;
+        left = right = null;
+    }
+}
