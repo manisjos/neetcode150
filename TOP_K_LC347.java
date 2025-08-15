7fb2a294dd6b6e6b81cde4ed2ac5c685a12e1472
manisjos <manish.joshi.d@gmail.com>
2025-08-15 07:45:50 +0530

    <><lc 347 Top K, Heap approach>

--- TOP_K_LC347.java
+++ TOP_K_LC347.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TOP_K_LC347 {
     public static void main(String[] args) {
+        System.out.println("Via Naive Approach");
         System.out.println((topKFrequent_Naive(new int[]{1,1,1,2,2,3},2)));
+        System.out.println("Via Heap Approach");
+        System.out.println(Arrays.toString(topK_Via_Heap(new int[]{1,1,1,2,2,3},2)));
     }
 
     public static ArrayList<Integer> topKFrequent_Naive(int[] nums, int rank) {
@@ -14,6 +14,7 @@ public class TOP_K_LC347 {
             map.put(num,map.getOrDefault(num,0)+1);
         }
         ArrayList<int[]> freq = new ArrayList<>();
+        // moving the o/p to array list of integer arrays
         for (Map.Entry<Integer,Integer> entry : map.entrySet()){
             freq.add(new int[]{entry.getKey(),entry.getValue()});
         }
@@ -21,6 +22,28 @@ public class TOP_K_LC347 {
         ArrayList<Integer> res = new ArrayList<>();
         for (int i=0;i<rank;i++){
             res.add(freq.get(i)[0]);
+        }
+        return res;
+    }
+
+    public static int[] topK_Via_Heap(int[] nums, int k){
+//        This gets O(n log k) complexity instead of O(n log n).
+        Map<Integer,Integer> freqMap = new HashMap<>();
+        for (int num:nums){
+            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>((a,b)-> a.getValue()-b.getValue());
+
+        for (Map.Entry<Integer,Integer> en: freqMap.entrySet()){
+            minHeap.offer(en);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i=k-1;i>=0;i--){
+            res[i] = minHeap.poll().getKey();
         }
         return res;
     }
