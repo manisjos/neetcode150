94abebd14fee146b0bfc5ce8886ad6e1554c8177
manisjos <manish.joshi.d@gmail.com>
2025-08-16 08:00:43 +0530

    <><LC 215: kthLargest without using sorting>

---
+++ KthLargetst_LC215.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+public class KthLargetst_LC215 {
+
+    public static void main(String[] args) {
+        int[] arr = {12, 3, 5, 7, 19};
+        int K = 2;
+        System.out.println(kthLargest(arr, K));
+    }
+    public static int kthLargest(int arr[], int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // maintaining a mean heap, as we store elements
+        for (int val:arr){
+            pq.add(val);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
