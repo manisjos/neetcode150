1fff5ef57c601c4d5ca8db383ba4cee8b15a20c9
manisjos <manish.joshi.d@gmail.com>
2025-08-17 17:07:26 +0530

    <><LC 20: Easy One - Binary Search>

--- LC20_ValidParanthesis.java
+++ LC20_ValidParanthesis.java
@@ -7,6 +7,29 @@ public class LC20_ValidParanthesis {
             System.out.println("'" + Paran + "' is this valid ? " + isValid(Paran));
         }
 //        System.out.println("Are the paranthesis valid ? "+isValid("Saw"));
+        System.out.println("Simple Binary Search");
+        int[] inputArr = {-1, 0, 3, 5, 9, 12};
+        int Target = 9;
+        System.out.println("=================================");
+        System.out.println("=================================");
+        System.out.println("Simple Binary Search ==>" + SimpleBinarySearch(inputArr, Target));
+        System.out.println("Recursive Binary Search ==>" + search(inputArr, Target));
+
+
+    }
+
+    public static int search(int[] nums, int target) {
+        return binarySearchRecursive(nums, target, 0, nums.length - 1);
+    }
+
+    public static int binarySearchRecursive(int[] nums, int target, int left, int right) {
+        if (left > right) return -1;
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) return binarySearchRecursive(nums, target, mid + 1, right);
+        else return binarySearchRecursive(nums, target, left, mid - 1);
     }
 
     public static boolean isValid(String s) {
@@ -31,4 +54,21 @@ public class LC20_ValidParanthesis {
         }
         return st.isEmpty();
     }
+
+    public static int SimpleBinarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
