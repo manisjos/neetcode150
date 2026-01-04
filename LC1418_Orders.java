6c1eaa9cab133de8a460a7c978b07908c262363e
manisjos <manish.joshi.d@gmail.com>
2026-01-04 12:13:10 +0530

    <><LC1418: Food Order>

--- LC1418_Orders.java
+++ LC1418_Orders.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class LC1418_Orders {
     public static void main(String[] args) {
@@ -11,6 +12,42 @@ public class LC1418_Orders {
                 List.of("Rous", "3", "Jain Ceviche")
         );
         System.out.println("Printing Order Table: \n" + displayTable(orders));
+        System.out.println("Printing Order Table Using Stream: \n" + displayTableUsingStream(orders));
+    }
+
+    private static List<List<String>> displayTableUsingStream(List<List<String>> orders) {
+        List<String> sortedItems = orders.stream()
+                .map(o -> o.get(2))
+                .distinct()
+                .sorted()
+                .toList();
+
+        Map<Integer, Map<String, Long>> tableFoodCount =
+                orders.stream()
+                        .collect(Collectors.groupingBy(
+                                o
+                                        -> Integer.parseInt(o.get(1)),
+                                TreeMap::new,
+                                Collectors.groupingBy(o -> o.get(2),
+                                        Collectors.counting())
+                        ));
+        List<List<String>> res = new ArrayList<>();
+        List<String> headers = new ArrayList<>();
+        headers.add("Table");
+        headers.addAll(sortedItems);
+        res.add(headers);
+
+        for (Map.Entry<Integer, Map<String, Long>> entry : tableFoodCount.entrySet()) {
+            List<String> row = new ArrayList<>();
+            row.add(String.valueOf(entry.getKey()));
+
+            Map<String, Long> foodCount = entry.getValue();
+            for (String item : sortedItems) {
+                row.add(String.valueOf(foodCount.getOrDefault(item, 0L)));
+            }
+            res.add(row);
+        }
+        return res;
     }
 
     public static List<List<String>> displayTable(List<List<String>> orders) {
