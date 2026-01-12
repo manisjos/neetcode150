8c31b6bb3307c6c08230fc55072715da6cb1484a
manisjos <manish.joshi.d@gmail.com>
2026-01-12 08:18:09 +0530

    <><LC1418: Food Order>

---
+++ LC1247.java
@@ -0,0 +1,22 @@
+public class LC1247 {
+    public static void main(String[] args) {
+        System.out.println("minimumSwapsNeeded -> " + minimumSwapsNeeded("xx", "yy"));
+    }
+
+    public static int minimumSwapsNeeded(String s1, String s2) {
+        int xy = 0, yx = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            char a = s1.charAt(i), b = s2.charAt(i);
+            if (a < b) {
+                ++xy;
+            }
+            if (a > b) {
+                ++yx;
+            }
+        }
+        if (xy + yx % 2 == 1) {
+            return -1;
+        }
+        return xy / 2 + yx / 2 + xy % 2 + yx % 2;
+    }
+}
