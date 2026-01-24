d1f433e12729aae3733d324e7a6f2cce9e6e4cd1
manisjos <manish.joshi.d@gmail.com>
2026-01-24 16:02:58 +0530

    <><LC1877>

--- ProdArr.java
+++ ProdArr.java
@@ -4,8 +4,33 @@ public class ProdArr {
     public static void main(String[] args) {
         System.out.println("First approach: ");
         System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
+        System.out.println("NEW approach: ");
+        System.out.println(Arrays.toString(prodExceptSelf(new int[]{1, 2, 3, 4})));
         System.out.println("Second approach: ");
         System.out.println(Arrays.toString(productExceptSelfPS(new int[]{1, 2, 3, 4})));
+    }
+
+    static int[] prodExceptSelf(int nums[]) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Step 1: Calculate prefix products
+        // result[i] will contain the product of all elements to the left of i
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: Calculate suffix products on the fly
+        // Multiply the prefix product by the product of all elements to the right
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
     }
 
     private static int[] productExceptSelfPS(int[] nums) {
--- miniMax_LC1877.java
+++ miniMax_LC1877.java
@@ -1,9 +1,23 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class miniMax_LC1877 {
     public static void main(String[] args) {
         int arr[] = {4, 1, 5, 1, 2, 5, 1, 5, 5, 4};
         System.out.println(minPairSum(arr));
+
+
+        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> map1 = new LinkedHashMap<>();
+        Map<String,Integer> map2 = new TreeMap<>();
+
+
+        map.put("Swasti",1);    map.put(null,22);map.put(null,23);    map.put("Jishant",12);   map.put("Krishvi",7);
+        map1.put("Swasti",1);     map1.put(null,1);    map1.put("Jishant",12);   map1.put("Krishvi",7);
+           map2.put("Swasti",1);        map2.put("Jishant",12);   map2.put("Krishvi",7);
+
+        System.out.println("map: "+map);
+        System.out.println("map: "+map1);
+        System.out.println("map: "+map2);
     }
 
     public static int minPairSum(int[] nums) {
