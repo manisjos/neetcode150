01662bd237b5abb6081dab4d02ce910693c273e4
manisjos <manish.joshi.d@gmail.com>
2026-01-25 08:09:16 +0530

    <><LC41>

--- firstMissingNumber_LC41.java
+++ firstMissingNumber_LC41.java
@@ -28,6 +28,49 @@ public class firstMissingNumber_LC41 {
         return -1; // never reached
     }
 
+    static int firstMissingPositiveOptimal(int[] nums) {
+        // each number x should be at x-1 index
+        // for eg number 1 should be at 0 index
+        // for eg number 2 should be at 1 index
+        // for eg number 3 should be at 2 index
+        // for eg number 4 should be at 3 index
+        // for eg number 5 should be at 4 index
+        // After rearranging - the ideal order
+//        Index: 0 1 2 3
+//        Value: 1 2 3 4
+//        If this is true → answer = n+1.
+//        If not → first mismatch is answer.
+
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                // checking if the number is in 1 to n+1 range only, then checking
+                // if the number is at expected place
+                // if (NOT) then the below swap happens
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+//        below is checking
+//        Index 0 → should be 1
+//        Index 1 → should be 2
+//        Index 2 → should be 3
+//        First mismatch = missing.
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    static void swap(int arr[], int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
     // For testing
     public static void main(String[] args) {
 
@@ -39,5 +82,15 @@ public class firstMissingNumber_LC41 {
 
         int[] nums3 = {1, 2, 0};
         System.out.println(firstMissingPositiveHMApproach(nums3)); // 3
+
+
+        System.out.println("== Via Optimal Approach ==");
+
+
+        System.out.println(firstMissingPositiveOptimal(nums1)); // 2
+
+        System.out.println(firstMissingPositiveOptimal(nums2)); // 1
+
+        System.out.println(firstMissingPositiveOptimal(nums3)); // 3
     }
 }
