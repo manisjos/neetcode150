1a2fe7288449316b3802abea2c3c4800ba221c25
manisjos <manish.joshi.d@gmail.com>
2026-01-30 23:02:30 +0530

    <><InsertionSort>

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,25 +3,40 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int arr[] = {5,8,12,1,7};
+        int arr[] = {5, 8, 12, 1, 7};
         System.out.println(Arrays.toString(boobaSort(arr)));
+        System.out.println(Arrays.toString(insertionSort(arr)));
+    }
+
+    private static int[] insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+        return arr;
     }
 
     private static int[] boobaSort(int[] arr) {
         int n = arr.length;
         boolean swapped;
-        for (int i=0;i<n-1;i++){
+        for (int i = 0; i < n - 1; i++) {
             swapped = false;
-            for (int j=0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     // swap
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     swapped = true;
                 }
             }
-            if(!swapped) break;
+            if (!swapped) break;
         }
         return arr;
     }
