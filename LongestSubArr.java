01f3459226414309ea3f6d69623ea88605d696dc
manisjos <manish.joshi.d@gmail.com>
2026-02-01 09:33:53 +0530

    <><printCountOfSubArrWhoSumAreK>

--- LongestSubArr.java
+++ LongestSubArr.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class LongestSubArr {
     public static void main(String[] args) {
-        System.out.println("Max Length Array Possible: "+maxSubArr(new int[]{1,2,3},3));
+        System.out.println("Max Length Array Possible: "+maxSubArr(new int[]{2, 1, 5, 1, 3, 2},7));
     }
 
     private static int maxSubArr(int[] nums, int k) {
@@ -13,7 +13,7 @@ public class LongestSubArr {
         int sumSoFar = 0, maxLen = 0;
         for (int i=0;i< nums.length;i++){
             sumSoFar = sumSoFar + nums[i];
-            if(map.containsKey(sumSoFar-k)){
+            if(map.containsKey(sumSoFar-k)){ // using the same two sum funda
                 maxLen = Math.max(maxLen,i- map.get(sumSoFar-k));
             }
             if(!map.containsKey(sumSoFar)){
--- MaxSumKSubArr.java
+++ MaxSumKSubArr.java
@@ -1,16 +1,73 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaxSumKSubArr {
     public static void main(String[] args) {
-//        int arr[] = {2, 1, 5, 1, 3, 2};
-        int arr[] = {4, 4, 4};
-        int k = 3;
+        int arr[] = {1, 5, 4, 2, 9, 9, 9};
+//        int arr[] = {4, 4, 4};
+        int k = 6;
         maxSumKSubArr(arr, k);
-        maximumSubarraySum(arr,k);
-        maximumSubarrayDistinctSum(arr,k);
+        maximumSubarraySum(arr, k);
+        maximumSubarrayDistinctSum(arr, k);
+        printCountOfSubArrWhoSumAreK(arr, k);
+    }
+
+    static void printCountOfSubArrWhoSumAreK(int arr[], int k) {
+        int count = 0, sumSoFar = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // to add the single entry, that will be fetched for first match in  line 21
+        for (int i = 0; i < arr.length; i++) {
+            sumSoFar += arr[i];
+            if (map.containsKey(sumSoFar - k)) {
+                count += map.get(sumSoFar - k);
+            }
+            map.put(sumSoFar, map.getOrDefault(sumSoFar, 0) + 1);
+        }
+        System.out.println("Printing Count Of Sub Arr Whos Sum Are K : "+count);
     }
 
     private static void maximumSubarrayDistinctSum(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) System.out.println("0");
+        long maxSum = 0, currSum = 0;
+        // Map <Element,Freq>
+        Map<Integer, Integer> countsMap = new HashMap<>();
+        // 1. process the first window
+        for (int i = 0; i < k; i++) {
+            currSum += arr[i];
+            countsMap.put(arr[i], countsMap.getOrDefault(arr[i], 0) + 1);
+        }
+        // check if first window is valid
+        if (countsMap.size() == k) {
+            maxSum = currSum;
+        }
+
+        // 2. Slide the window through the rest of the array
+        for (int i = k; i < n; i++) {
+            // add the incoming element
+            int incoming = arr[i];
+            currSum += incoming;
+            countsMap.put(incoming, countsMap.getOrDefault(incoming, 0) + 1);
+
+            // remove the outgoing element
+            int outgoing = arr[i - k];
+            currSum -= outgoing;
+
+            if (countsMap.get(outgoing) == 1) {
+                countsMap.remove(outgoing);
+            } else {
+                countsMap.put(outgoing, countsMap.get(outgoing) - 1);
+            }
+
+            if (countsMap.size() == k) {
+                maxSum = Math.max(maxSum, currSum);
+            }
+
+        }
+        System.out.println("Max Distinct Sum: " + maxSum);
 
     }
+
     private static void maxSumKSubArr(int[] arr, int k) {
         int n = arr.length, currSum = 0, maxSum = Integer.MIN_VALUE;
         if (n > 3) {
@@ -47,6 +104,6 @@ public class MaxSumKSubArr {
             windowSum = windowSum + arr[i] - arr[i - k];
             maxSum = Math.max(maxSum, windowSum);
         }
-        System.out.println("Max: "+maxSum);
+        System.out.println("Max: " + maxSum);
     }
 }
