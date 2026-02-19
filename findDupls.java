8640aa9ef09b26c46fe3061cc65fac303a0e53b6
manisjos <manish.joshi.d@gmail.com>
2026-02-19 11:32:26 +0530

    <><LC287>

--- findDupls.java
+++ findDupls.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class findDupls {
     public static void main(String[] args) {
         int[] arr = {3, 1, 4, 3, 2};
-        System.out.println("This doesnt work" + findDup(arr));
+        System.out.println("Naive and Costly way: " + naiveAndCostlyWay(arr));
         System.out.println("Best Solution: " + findDupFloydAlgo(arr));
     }
 
@@ -20,7 +22,8 @@ public class findDupls {
         return slow;
     }
 
-    static int findDup(int arr[]) {
+    static int naiveAndCostlyWay(int arr[]) {
+        Arrays.sort(arr);
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
