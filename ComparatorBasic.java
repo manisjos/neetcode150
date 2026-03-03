a0c78c7b0d8725dabb4f71fd61089ed6b128afed
manisjos <manish.joshi.d@gmail.com>
2026-03-03 22:01:48 +0530

    <><comparator old style simple>

--- ComparatorBasic.java
+++ ComparatorBasic.java
@@ -20,6 +20,15 @@ public class ComparatorBasic {
                 new Student(7, "Ramos", 40)
 
         ));
+
+        List<Student> lambdaList = new ArrayList<>(Arrays.asList(
+                new Student(11, "Xabi", 22.0),
+                new Student(21, "Jis", 20),
+                new Student(1, "Swas", 70),
+                new Student(6, "Kris", 50),
+                new Student(7, "Ramos", 40)
+
+        ));
         System.out.println("Comparable Way: ");
         System.out.println("Before "+ el.stream().map(Student::getMarks).toList());
         Collections.sort(el);
@@ -31,6 +40,30 @@ public class ComparatorBasic {
         c2.sort(new AgeComparator());
         System.out.println("After "+ c2.stream().map(Student::getName).toList());
 
+        System.out.println("Lambda Way: ");
+
+        System.out.println("Before "+ lambdaList.stream().map(Student::getName).toList());
+        // Lambda — cleanest form
+        lambdaList.sort((a,b)->a.getName().compareTo(b.getName()));
+
+        // Method reference
+        lambdaList.sort(Comparator.comparing(Student::getName));
+
+        // Reverse order
+        lambdaList.sort(Comparator.comparing(Student::getRollNum).reversed());
+        System.out.println("After "+ lambdaList.stream().map(Student::getName).toList());
+
+
+        // Chain multiple sorts
+        lambdaList.sort(
+                Comparator.comparing(Student::getName)
+                        .thenComparing(Student::getRollNum)
+                        .thenComparing(Student::getMarks)
+        );
+
+        // Null-safe
+        Comparator.nullsFirst(Comparator.comparing(Student::getName));
+        Comparator.nullsLast(Comparator.naturalOrder());
 
     }
 }
