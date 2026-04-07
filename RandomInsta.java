6c8553e535907e1c16935f632d0a052b560fc641
manisjos <manish.joshi.d@gmail.com>
2026-04-07 21:12:54 +0530

    <><random>

--- RandomInsta.java
+++ RandomInsta.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RandomInsta {
     public static void main(String[] args) {
@@ -25,6 +23,39 @@ public class RandomInsta {
 //                .filter(x->!x.isEmpty())
                         .collect(Collectors.toList());
         System.out.println(filteredNames);
+        int arr[] = {14, 5, 63, 3, 2, 44};
+        findLargestSecondLargest(arr);
 
+        List<Integer> l1 = Arrays.asList(1,2,3,4,5);
+        List<Integer> l2 = Arrays.asList(4,5,6,7,8);
+        removeDuplicatesAndMerge(l1,l2);
+    }
+
+    private static void removeDuplicatesAndMerge(List<Integer> l1, List<Integer> l2) {
+        List<Integer> result =
+        Stream.concat(l1.stream(),l2.stream()).distinct().toList();
+        System.out.println("fixed -> "+result);
+
+        String s = "Good Morning";
+        Optional<String> len = Stream.of(s.split(" "))
+                .max(Comparator.comparingInt(String::length));
+        System.out.println(len.get()); // printing the word with highest length
+    }
+
+    private static void findLargestSecondLargest(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if(arr[i]>secondLargest && arr[i]!=largest){
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest : "+largest+", SecondLargest: "+secondLargest);
     }
 }
