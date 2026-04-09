30a82b99ea4b3a3769f87b4d1f1b6ed444d19d5b
manisjos <manish.joshi.d@gmail.com>
2026-04-09 08:00:25 +0530

    <><LC844>

--- LC844_String.java
+++ LC844_String.java
@@ -1,9 +1,66 @@
+import java.util.Stack;
+
 public class LC844_String {
     public static void main(String[] args) {
         String s = "ab#c";
         String t = "ad#c";
 
         System.out.println(backspaceCompare(s, t));
+        System.out.println("Optimal way: ");
+        System.out.println(backspaceCompareOptimal2Pointer(s, t));
+    }
+
+    private static boolean backspaceCompareOptimal2Pointer(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else break;
+            }
+
+
+            while (j >= 0) {
+                if (s.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else break;
+            }
+
+            // Compare actual characters
+            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) return false;
+            if ((i >= 0) != (j >= 0)) return false;
+            i--; j--;
+
+        }
+        return true;
+    }
+
+    private static String solveUsingStack(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '#') {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb = new StringBuilder("");
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 
     private static boolean backspaceCompare(String s, String t) {
@@ -13,7 +70,7 @@ public class LC844_String {
 
 
         String sb = solve(s);
-        String st = solve(t);
+        String st = solveUsingStack(t);
         if (sb.equals(st)) return true;
         else return false;
     }
