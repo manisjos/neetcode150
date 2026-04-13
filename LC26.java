14f839554fb990bb9891b52cd9273850c6330e0f
manisjos <manish.joshi.d@gmail.com>
2026-04-13 08:04:40 +0530

    <><LC26>

--- LC26.java
+++ LC26.java
@@ -8,9 +8,10 @@ public class LC26 {
         if (arr.length == 0) return 0;
         int left = 0;
         for (int right = 1; right < arr.length; right++) {
-            if (arr[right] != arr[left]) {
+            if (arr[right] != arr[left]) { // case when they are actually diff - and we know we have 1 repeated
+
                 left++; // move slow pointer
-                arr[left] = arr[right]; // overwrite duplicates
+                arr[left] = arr[right]; // overwrite duplicate - with unique elements
             }
         }
         return left + 1; // number of unique elements
