d85041bc3770a0e92f1598a679720c426582bb13
manisjos <manish.joshi.d@gmail.com>
2026-04-30 08:29:27 +0530

    <><Sorting 912>

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,9 +4,37 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         int arr[] = {5, 8, 12, 1, 7};
+        int arr1[] = {5, 8, 12, 1, 7};
         System.out.println(Arrays.toString(boobaSort(arr)));
         System.out.println(Arrays.toString(insertionSort(arr)));
-        System.out.println("Simple Bubble: "+Arrays.toString(simpleBubbleSort(arr)));
+        System.out.println("Simple Bubble: " + Arrays.toString(simpleBubbleSort(arr)));
+        System.out.println("Merge Bubble: " + Arrays.toString(mergeSort(arr1)));
+    }
+
+    private static int[] mergeSort(int[] arr) {
+        mergeSortCustom(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private static void mergeSortCustom(int[] arr, int left, int right) {
+        if (left >= right) return; // base case : 1 element
+        int mid = left + (right - left) / 2; // avoids int overflow vs (l+r) / 2
+        mergeSortCustom(arr, left, mid);
+        mergeSortCustom(arr, mid + 1, right);
+        actualMerge(arr, left, mid, right);
+    }
+
+    private static void actualMerge(int[] arr, int left, int mid, int right) {
+        int[] tmp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right)
+            tmp[k++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+
+        while (i <= mid) tmp[k++] = arr[i++];
+        while (j <= right) tmp[k++] = arr[j++];
+
+        System.arraycopy(tmp, 0, arr, left, tmp.length);
     }
 
     private static int[] simpleBubbleSort(int[] arr) {
