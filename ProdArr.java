731df04aef1838b31c605536ce3177091d68f692
manisjos <manish.joshi.d@gmail.com>
2026-07-24 08:01:06 +0530

    <comeback><ProdArr>

--- ProdArr.java
+++ ProdArr.java
@@ -2,12 +2,56 @@ import java.util.Arrays;
 
 public class ProdArr {
     public static void main(String[] args) {
+        System.out.println("BruteForce: ");
+        System.out.println(Arrays.toString(prodArrBF(new int[]{1, 2, 3, 4})));
+        System.out.println("moreOptimal: ");
+        System.out.println(Arrays.toString(moreOptimal(new int[]{1, 2, 3, 4})));
         System.out.println("First approach: ");
         System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
         System.out.println("NEW approach: ");
         System.out.println(Arrays.toString(prodExceptSelf(new int[]{1, 2, 3, 4})));
         System.out.println("Second approach: ");
         System.out.println(Arrays.toString(productExceptSelfPS(new int[]{1, 2, 3, 4})));
+    }
+
+    static int[] prodArrBF(int arr[]) {
+//        Time  : O(n²)
+//        Space : O(1)
+
+        int result[] = new int[arr.length];
+        for (int currIdx = 0; currIdx < arr.length; currIdx++) {
+            int prod = 1;
+            for (int otherIdx = 0; otherIdx < arr.length; otherIdx++) {
+                if (currIdx != otherIdx) { // excluding the current index and getting the multiplication done
+                    prod *= arr[otherIdx];
+                }
+            }
+            result[currIdx] = prod;
+        }
+        return result;
+    }
+
+    static int[] moreOptimal(int arr[]) {
+        int len = arr.length;
+        int[] leftProds = new int[len];
+        int[] rightProds = new int[len];
+        int[] ans = new int[len];
+
+        leftProds[0] = 1;
+
+        for (int idx = 1; idx < len; idx++) {
+            leftProds[idx] = leftProds[idx - 1] * arr[idx - 1];
+        }
+
+        rightProds[len - 1] = 1;
+        for (int idx = len - 2; idx >= 0; idx--) {
+            rightProds[idx] = rightProds[idx + 1] * arr[idx + 1];
+        }
+
+        for (int idx = 0; idx < len; idx++) {
+            ans[idx] = leftProds[idx] * rightProds[idx];
+        }
+        return ans;
     }
 
     static int[] prodExceptSelf(int nums[]) {
