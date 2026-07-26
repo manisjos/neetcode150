37825a6c2029082645ae688dd3b2be230f1005cb
manisjos <manish.joshi.d@gmail.com>
2026-07-26 08:02:59 +0530

    <comeback><Sorted>

---
+++ SortedRotated.java
@@ -0,0 +1,36 @@
+public class SortedRotated {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int element = 6;
+        System.out.println("__ >> "+search(arr, element));
+    }
+
+    static int search(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[left] <= arr[mid]) {
+                if (target >= arr[left] && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    right = mid + 1;
+                }
+            } else {
+                if (target > arr[mid] && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+
+        }
+        return -1;
+    }
+}
