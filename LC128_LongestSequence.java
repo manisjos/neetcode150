9732c26cc4b80953d9d1d7a92a82db4c1d5333d6
manisjos <manish.joshi.d@gmail.com>
2026-07-28 14:42:12 +0530

    <comeback><IsPalin>

--- LC128_LongestSequence.java
+++ LC128_LongestSequence.java
@@ -4,7 +4,10 @@ import java.util.Set;
 public class LC128_LongestSequence {
     public static void main(String[] args) {
         int arr[] = {100, 4, 200, 1, 3, 2};
-        System.out.println("The longest streak in current array is "+longestConsecutive(arr)+".");
+        System.out.println("The longest streak in current array is " + longestConsecutive(arr) + ".");
+
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println("Is Valid Palindrome ? " + isPalin(s));
     }
 
     static int longestConsecutive(int arr[]) {
@@ -35,9 +38,30 @@ public class LC128_LongestSequence {
     }
 
 
-    public boolean isPalin(String s){
-        if(s==null)
+    static boolean isPalin(String s) {
+        if (s == null)
             return false;
 
+
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            // Skip non-alphanumeric characters from the left
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            // Skip non-alphanumeric characters from the right
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            //compare lowercased characters
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
