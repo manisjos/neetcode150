e47cf4a80b61cc4f28f00b4735c999ff5c8a919e
manisjos <manish.joshi.d@gmail.com>
2026-07-29 08:04:03 +0530

    <comeback><DailyTemps>

--- DailyTemperature.java
+++ DailyTemperature.java
@@ -4,6 +4,30 @@ public class DailyTemperature {
     public static void main(String[] args) {
         int arr[] = {73, 74, 75, 71, 69, 72, 76, 73};
         System.out.println("Array of Waits (BruteForce) : " + Arrays.toString(dailyWait(arr)));
+        System.out.println("Array of Waits (Optimal) : " + Arrays.toString(dailyWaitOptimal(arr)));
+    }
+
+    static int[] dailyWaitOptimal(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        // Use an array to simulate stack mechanics
+        int[] stack = new int[n];
+        int top = -1;
+
+        for (int currentDay = 0; currentDay < n; currentDay++) {
+            // Resolve all previous colder days
+            while (top >= 0 && temperatures[currentDay] > temperatures[stack[top]]) {
+                int previousDay = stack[top--]; // pop
+                answer[previousDay] = currentDay - previousDay;
+            }
+
+            // Push current day onto stack
+            stack[++top] = currentDay;
+        }
+
+        return answer;
     }
 
     static int[] dailyWait(int temperatures[]) {
