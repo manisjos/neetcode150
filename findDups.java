95b374b25fc22054dd13ad96dc40a8fce469f538
manisjos <manish.joshi.d@gmail.com>
2026-08-14 07:55:46 +0530

    <><>

--- findDups.java
+++ findDups.java
@@ -5,6 +5,33 @@ public class findDups {
 
         System.out.println("Duplicate in nums1: " + findDuplicate(nums1)); // Output: 2
         System.out.println("Duplicate in nums2: " + findDuplicate(nums2)); // Output: 3
+
+
+        System.out.println("Duplicate in nums1: " + findDuplicateBS(nums1)); // Output: 2
+        System.out.println("Duplicate in nums2: " + findDuplicateBS(nums2)); // Output: 3
+    }
+
+    static int findDuplicateBS(int arr[]) {
+        int low = 1;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+
+            for (int num : arr) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+
+            if (count > mid) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 
     static int findDuplicate(int arr[]) {
